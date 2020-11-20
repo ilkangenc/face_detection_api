5239c6618d22aadc883a15e8bc71017e70adb6bd
ilkangenc <gencilkan@gmail.com>
2020-11-20 14:19:10 +0300

    added api

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const signin = require('./controllers/signin');
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '',
+    database : 'facedetection'
+  }
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req,res)=> {res.send('success')})
+
+app.post('/signin', (req,res) => {signin.handleSignin(req,res,db,bcrypt)})
+
+app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)})
+
+app.get('/profile/:id',(req,res) => {profile.handleProfileGet(req,res,db)})
+
+app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
+
+app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
+
+app.listen(process.env.PORT || 3000, () => {console.log(`app is running on port ${process.env.PORT}`)})
